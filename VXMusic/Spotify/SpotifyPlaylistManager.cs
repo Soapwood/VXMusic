3de6b215e66463ed546037c8309d2a05f2d7a48d
Soapwood <pwezem@gmail.com>
2023-08-15 00:34:08 +0100

    Move playlist creation and addition logic out to the Spotify Playlist Manager

--- VXMusic/Spotify/SpotifyPlaylistManager.cs
+++ VXMusic/Spotify/SpotifyPlaylistManager.cs
@@ -2,30 +2,56 @@ using SpotifyAPI.Web;
 
 namespace VXMusic.Spotify;
 
+using Disco.Business.Interfaces.Dtos.AudD;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 public class SpotifyPlaylistManager
 {
-    public async Task<string> CreatePlaylist(string userId, string playlistName, bool isPublic = true)
+    public static async Task<FullPlaylist> CreatePlaylist(string userId, string playlistName, bool isPublic = true)
     {
         if (SpotifyClientBuilder.Instance == null)
-            throw new Exception("Not authenticated with Spotify API.");
+            throw new Exception("Not authenticated with Spotify API.");  // TODO Handle this, and output to UI
 
-        //var newPlaylist = await SpotifyClientBuilder.Instance.Playlists.Create()CreatePlaylistAsync(userId, playlistName, publicPlaylist: isPublic);
-        //return newPlaylist.Id;
-        return null;
+        var spotify = await SpotifyClientBuilder.Instance;
+
+        var newPlaylist = new PlaylistCreateRequest(playlistName);
+
+        // TODO Make this injectable
+        newPlaylist.Description = "Made by VXMusic.";
+        newPlaylist.Collaborative = false;
+        newPlaylist.Public = false;
+
+        FullPlaylist fullPlaylist = await spotify.Playlists.Create(userId, newPlaylist);
+
+        return fullPlaylist;
     }
 
-    public async Task<bool> AddTrackToPlaylist(string playlistId, string trackUri)
+    public static async Task<bool> AddTrackToPlaylist(string playlistId, PlaylistAddItemsRequest playlistAddItemsRequest)
     {
         if (SpotifyClientBuilder.Instance == null)
-            throw new Exception("Not authenticated with Spotify API.");
+            throw new Exception("Not authenticated with Spotify API."); // TODO Handle this, and output to UI
 
-        var uris = new List<string> { trackUri };
-        //var snapshotId = await SpotifyClientBuilder.Instance.AddPlaylistTracksAsync(playlistId, uris);
-        //return !string.IsNullOrEmpty(snapshotId);
-        return false; 
+        var spotify = await SpotifyClientBuilder.Instance;
+
+        var itemAddResponse = await spotify.Playlists.AddItems(playlistId, playlistAddItemsRequest);
+        return itemAddResponse != null;
+    }
+
+    public static string GetPlaylistIdByNameIfExists(string playlistName, IList<SimplePlaylist> playlists)
+    {
+        if (SpotifyClientBuilder.Instance == null)
+            throw new Exception("Not authenticated with Spotify API."); // TODO Handle this, and output to UI
+
+        foreach(var playlist in playlists)
+        {
+            if(playlist.Name == playlistName)
+            {
+                return playlist.Id.ToString();
+            }
+        }
+
+        return null;
     }
 }
