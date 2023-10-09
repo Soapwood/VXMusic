ee014ccc5e4aa639f8d566f75222a9ccad231319
Soapwood <pwezem@gmail.com>
2023-10-09 19:59:15 +0100

    Add SpotifyUserProfileManager - this will be used for checking if spotify is logged in

---
+++ VXMusic/Connections/Spotify/SpotifyUserProfileManager.cs
@@ -0,0 +1,23 @@
+﻿using SpotifyAPI.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VXMusic.Spotify;
+
+namespace VXMusic.Connections.Spotify
+{
+    internal class SpotifyUserProfileManager
+    {
+        public static async Task<PrivateUser> GetCurrentUser()
+        {
+            //if (SpotifyClientBuilder.Instance == null)
+            //    throw new Exception("Not authenticated with Spotify API.");  // TODO Handle this, and output to UI
+
+            var spotify = await SpotifyClientBuilder.Instance;
+
+            return await spotify.UserProfile.Current();
+        }
+    }
+}
