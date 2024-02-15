4f9c91071f8d3bdb526acb0aee1d052013607a78
Soapwood <pwezem@gmail.com>
2024-02-15 22:24:50 +0000

    Make VXMMessages usable in unity project

--- VXMusic/Overlay/VXMMessages.cs
+++ VXMusic/Overlay/VXMMessages.cs
@@ -1,9 +1,13 @@
-namespace VXMusic.Overlay;
-
-public class VXMMessages
+namespace VXMusic.Overlay
 {
-    public static readonly string CONNECTION_ACKNOWLEDGE = "VX_CONNECT_ACK";
-    public static readonly string RECOGNITION_REQUEST = "VX_RECOGNITION_REQ";
-    public static readonly string RECOGNITION_ACKNOWLEDGE = "VX_RECOGNITION_ACK";
-    public static readonly string RECOGNITION_FINISH = "VX_RECOGNITION_FIN";
+    public class VXMMessages
+    {
+        public static readonly string CONNECTION_REQUEST = "VX_CONNECT_REQ";
+        public static readonly string CONNECTION_ACKNOWLEDGE = "VX_CONNECT_ACK";
+        public static readonly string CONNECTION_FINISH = "VX_CONNECT_FIN";
+        
+        public static readonly string RECOGNITION_REQUEST = "VX_RECOGNITION_REQ";
+        public static readonly string RECOGNITION_ACKNOWLEDGE = "VX_RECOGNITION_ACK";
+        public static readonly string RECOGNITION_FINISH = "VX_RECOGNITION_FIN";
+    }
 }
