5655b28dcf5af0b144aac2e66837801cce4a538d
Soapwood <pwezem@gmail.com>
2024-06-10 23:50:52 +0100

    Fix path to Base64 Spotify and Last.fm Icons. Update to version 0.6.0

--- VXMusicDesktop/Images/IconImages.cs
+++ VXMusicDesktop/Images/IconImages.cs
@@ -6,7 +6,7 @@ namespace VXMusicDesktop.Images;
 public class IconImages
 {
     public static string SpotifyLogo = File.ReadAllText(
-        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", "SpotifyIconBase64.txt"));
+        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Overlay", "Images", "SpotifyIconBase64.txt"));
     public static string LastFmLogo = File.ReadAllText(
-        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", "LastFmIconBase64.txt"));
+        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Overlay", "Images", "LastFmIconBase64.txt"));
 }
