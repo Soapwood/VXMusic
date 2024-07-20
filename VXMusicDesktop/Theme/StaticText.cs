f26383f96d1e5116b4ca63b1f732767fc39982a8
Soapwood <pwezem@gmail.com>
2024-07-20 21:08:30 +0100

    Bump to 0.6.4.0! Update Tip text.

--- VXMusicDesktop/Theme/StaticText.cs
+++ VXMusicDesktop/Theme/StaticText.cs
@@ -1,11 +1,13 @@
+using System;
+
 namespace VXMusicDesktop.Theme;
 
 public class StaticText
 {
-    public static readonly string VxTipsText = """
-    - Install as SteamVR Overlay in Settings.
-    - Launch Overlay by Default in Setting.
-    - Check for new Nightly Builds from the update button on the bottom right.
-    - Optionally show VRChat notifications!
+    public static readonly string VxTipsText = $"""
+    - Install VXMusic as an Overlay in Settings so it can launch with SteamVR!
+    - Ensure Launch Overlay by Default in Settings is enabled.
+    - Try Nightly Builds from the update button on the bottom right.
+    - Optionally show recognition notifications in VRChat!
     """;
 }
