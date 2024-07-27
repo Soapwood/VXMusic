a7e137306f1021e6b1a7cfa5eaaed9b6a71a23e6
Soapwood <pwezem@gmail.com>
2024-07-27 19:54:35 +0100

    Add button to About view to open logs folder for when users are reporting bugs.

--- VXMusicDesktop/MVVM/ViewModel/AboutViewModel.cs
+++ VXMusicDesktop/MVVM/ViewModel/AboutViewModel.cs
@@ -9,21 +9,44 @@ using System.Windows;
 using VXMusicDesktop.Core;
 using System.Windows.Input;
 using System.ComponentModel;
+using System.IO;
 
 namespace VXMusicDesktop.MVVM.ViewModel
 {
     public class AboutViewModel : INotifyPropertyChanged
     {
+        private RelayCommand openFolderClick;
+        public ICommand OpenFolderCommand => openFolderClick ??= new RelayCommand(OpenFolder);
+
         public event PropertyChangedEventHandler? PropertyChanged;
+        
+        private static readonly string LogsOutputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "VirtualXtensions", "VXMusic", "Logs");
         
         public AboutViewModel()
         {
-            
         }
 
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        private void OpenFolder(object commandParameter)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = LogsOutputPath,
+                    UseShellExecute = true,
+                    Verb = "open"
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open Log folder: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
